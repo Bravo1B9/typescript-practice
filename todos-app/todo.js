6435d4eb72e07268eab3afc1735a9ff09d4ae06c
Bravo1B9 <b1@xgmi.com>
2023-01-03 13:57:14 +0000

    Initial todo list app

---
+++ todos-app/todo.js
@@ -0,0 +1,132 @@
+var Todo = /** @class */ (function () {
+    function Todo(id, title) {
+        var _this = this;
+        this.subTasks = [];
+        this.nextSubTaskId = 1;
+        this.numSubTasksCompleted = 0;
+        this.addSubTask = function (description) {
+            var newSubTask = new SubTask(_this.nextSubTaskId, description);
+            _this.subTasks.push(newSubTask);
+            _this.nextSubTaskId++;
+        };
+        this.getSubTaskById = function (id) {
+            for (var i = 0; i < _this.subTasks.length; i++) {
+                if (_this.subTasks[i].id == id) {
+                    console.log(_this.subTasks[i]);
+                }
+            }
+        };
+        this.completeSubTask = function (id) {
+            for (var i = 0; i < _this.subTasks.length; i++) {
+                if (_this.subTasks[i].id == id) {
+                    _this.subTasks[i].completed = true;
+                    _this.numSubTasksCompleted++;
+                }
+            }
+        };
+        this.id = id;
+        this.title = title;
+        this.completed = false;
+    }
+    return Todo;
+}());
+var SubTask = /** @class */ (function () {
+    function SubTask(id, description) {
+        this.id = id;
+        this.description = description;
+        this.completed = false;
+    }
+    return SubTask;
+}());
+var todos = [];
+var nextTodoId = 1;
+/*
+  Todo operations
+*/
+var createTodo = function (title) {
+    var newTodo = new Todo(nextTodoId, title);
+    todos.push(newTodo);
+    nextTodoId++;
+};
+var getTodoById = function (id) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            console.log(todos[i]);
+        }
+    }
+};
+var getAllTodos = function () {
+    console.log(todos);
+};
+var updateTodoTitleById = function (id, newTitle) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos[i].title = newTitle;
+        }
+    }
+};
+var removeTodoById = function (id) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos.splice(todos.indexOf(todos[i]), 1)[0];
+            todos.pop();
+        }
+    }
+};
+/*
+  SubTask operations
+*/
+var addSubTaskByTodoId = function (id, description) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos[i].addSubTask(description);
+        }
+    }
+};
+var getSubTaskByIds = function (todoId, subTaskId) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == todoId) {
+            todos[i].getSubTaskById(subTaskId);
+        }
+    }
+};
+var getAllSubTasks = function (todoId) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == todoId) {
+            console.log(todos[i].subTasks);
+        }
+    }
+};
+var updateSubTaskByIds = function (todoId, subTaskId, newDescription) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == todoId) {
+            for (var j = 0; j < todos[i].subTasks.length; j++) {
+                if (todos[i].subTasks[j].id == subTaskId) {
+                    todos[i].subTasks[j].description = newDescription;
+                }
+            }
+        }
+    }
+};
+var completeSubTask = function (todoId, subTaskId) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == todoId) {
+            todos[i].completeSubTask(subTaskId);
+            if (todos[i].numSubTasksCompleted == todos[i].subTasks.length) {
+                todos[i].completed = true;
+            }
+        }
+    }
+};
+var removeSubTaskByIds = function (todoId, subTaskId) {
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == todoId) {
+            for (var j = 0; j < todos[i].subTasks.length; j++) {
+                if (todos[i].subTasks[j].id == subTaskId) {
+                    todos[i].subTasks.splice(todos[i].subTasks.indexOf(todos[i].subTasks[j], 1)[0]);
+                    todos[i].subTasks.pop();
+                }
+            }
+        }
+    }
+};
---
+++ todos-app/todo.ts
@@ -0,0 +1,154 @@
+class Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  subTasks: SubTask[] = [];
+  nextSubTaskId: number = 1;
+  numSubTasksCompleted: number = 0;
+
+  constructor(id: number, title: string) {
+    this.id = id;
+    this.title = title;
+    this.completed = false;
+  }
+
+  addSubTask = (description: string) => {
+    const newSubTask = new SubTask(this.nextSubTaskId, description);
+    this.subTasks.push(newSubTask);
+    this.nextSubTaskId++;
+  }
+
+  getSubTaskById = (id: number) => {
+    for (let i = 0; i < this.subTasks.length; i++) {
+      if (this.subTasks[i].id == id) {
+        console.log(this.subTasks[i]);
+      }
+    }
+  }
+
+  completeSubTask = (id: number) => {
+    for (let i = 0; i < this.subTasks.length; i++) {
+      if (this.subTasks[i].id == id) {
+        this.subTasks[i].completed = true;
+        this.numSubTasksCompleted++;
+      }
+    }
+  }
+}
+
+class SubTask {
+  id: number;
+  description: string;
+  completed: boolean;
+
+  constructor(id: number, description: string) {
+    this.id = id;
+    this.description = description;
+    this.completed = false;
+  }
+}
+
+let todos: Todo[] = [];
+
+let nextTodoId: number = 1;
+
+/*
+  Todo operations
+*/
+const createTodo = (title: string) => {
+  const newTodo = new Todo(nextTodoId, title);
+  todos.push(newTodo);
+  nextTodoId++;
+}
+
+const getTodoById = (id: number) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      console.log(todos[i]);
+    }
+  }
+}
+
+const getAllTodos = () => {
+  console.log(todos);
+}
+
+const updateTodoTitleById = (id: number, newTitle: string) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos[i].title = newTitle;
+    }
+  }
+}
+
+const removeTodoById = (id: number) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos.splice(todos.indexOf(todos[i]), 1)[0];
+      todos.pop();
+    }
+  }
+}
+
+/*
+  SubTask operations
+*/
+const addSubTaskByTodoId = (id: number, description: string) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos[i].addSubTask(description);
+    }
+  }
+}
+
+const getSubTaskByIds = (todoId: number, subTaskId: number) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == todoId) {
+      todos[i].getSubTaskById(subTaskId);
+    }
+  }
+}
+
+const getAllSubTasks = (todoId: number) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == todoId) {
+      console.log(todos[i].subTasks);
+    }
+  }
+}
+
+const updateSubTaskByIds = (todoId: number, subTaskId: number, newDescription: string) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == todoId) {
+      for (let j = 0; j < todos[i].subTasks.length; j++) {
+        if (todos[i].subTasks[j].id == subTaskId) {
+          todos[i].subTasks[j].description = newDescription;
+        }
+      }
+    }
+  }
+}
+
+const completeSubTask = (todoId: number, subTaskId: number) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == todoId) {
+      todos[i].completeSubTask(subTaskId);
+      if (todos[i].numSubTasksCompleted == todos[i].subTasks.length) {
+        todos[i].completed = true;
+      }
+    }
+  }
+}
+
+const removeSubTaskByIds = (todoId: number, subTaskId: number) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == todoId) {
+      for (let j = 0; j < todos[i].subTasks.length; j++) {
+        if (todos[i].subTasks[j].id == subTaskId) {
+          todos[i].subTasks.splice(todos[i].subTasks.indexOf(todos[i].subTasks[j], 1)[0]);
+          todos[i].subTasks.pop();
+        }
+      }
+    }
+  }
+}
